5efe394a7f814ea6ad9412e0a3ce22dc5776f12a
Nowogrodzki <maciej.nowogrodzki.97@wp.pl>
2020-05-16 23:10:35 +0200

    exercise2 changes

--- exercise2.js
+++ exercise2.js
@@ -1,9 +1,11 @@
 // Exercise nr 2 
 
 const triangle = (a,b,c) => {
-    if(typeof a != 'number' || typeof b != 'number' || typeof c != 'number') throw new Error('Arguments have to be a numbers');
-    if(a < 0 || b < 0 || c < 0) throw new Error('All arguments have to be greater then 0');
-    return a + b > c ? `Długość krótszych boków a i b jest większa od C co oznacze ze jest git` : `to nie jest trójkąt prostokątny`;
+    const allArgumentsAreNumbers = typeof a != 'number' || typeof b != 'number' || typeof c != 'number';
+    const greateThenZero = a < 0 || b < 0 || c < 0
+    if(allArgumentsAreNumbers) throw new Error('Arguments have to be a numbers');
+    if(greateThenZero) throw new Error('All arguments have to be greater then 0');
+    return a + b > c ? `Długość krótszych boków a i b jest większa od C co oznacze trójkąt prostokątny` : `to nie jest trójkąt prostokątny`;
 }
 
 console.log(triangle(5,4,3));
