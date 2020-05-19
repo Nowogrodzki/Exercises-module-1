240c186e685c502aa5eb7d257ba62af6b8c93890
Nowogrodzki <maciej.nowogrodzki.97@wp.pl>
2020-05-19 15:10:21 +0200

    Another change to exercise nr2

--- exercise2.js
+++ exercise2.js
@@ -5,7 +5,7 @@ const triangle = (a,b,c) => {
     const greateThenZero = a < 0 || b < 0 || c < 0
     if(allArgumentsAreNumbers) throw new Error('Arguments have to be a numbers');
     if(greateThenZero) throw new Error('All arguments have to be greater then 0');
-    return a * a + b * b === c * c ? `Kwadrat najdłuższego boku jest równy sumie dwóch pozostałych boków co oznacza, że trójkąt jest prostokątny` : `to nie jest trójkąt prostokątny`;
+    return Math.pow(a,2) + Math.pow(b,2) === Math.pow(c, 2) ? `Kwadrat najdłuższego boku jest równy sumie dwóch pozostałych boków co oznacza, że trójkąt jest prostokątny` : `to nie jest trójkąt prostokątny`;
 }
 
 console.log(triangle(3,4,5));
