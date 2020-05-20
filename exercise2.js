3b0ce377b0a1e146b8c0db36fa43c2ec208f1a21
Nowogrodzki <maciej.nowogrodzki.97@wp.pl>
2020-05-20 14:51:31 +0200

    exercise nr2 changes

--- exercise2.js
+++ exercise2.js
@@ -1,17 +1,16 @@
 // Exercise nr 2 
 
-const triangle = function (a,b,c) {
-    const allArgumentsAreNumbers = typeof a != 'number' || typeof b != 'number' || typeof c != 'number';
-    const greateThenZero = a < 0 || b < 0 || c < 0
-    arguments = Array.from(arguments).sort((a,b) => a - b);
+const isRectangularTriangle = function (x1,x2,x3) {
+    const allArgumentsAreNumbers = Array.from(arguments).some(item => typeof item != 'number')
+    const greateThenZero = Array.from(arguments).some(item => item < 0);
+    const [a,b,c] = Array.from(arguments).sort((a,b) => a - b);
     if(allArgumentsAreNumbers) {
         throw new Error('Arguments have to be a numbers');
     }
     if(greateThenZero) {
         throw new Error('All arguments have to be greater then 0');
     }
-    return Math.pow(arguments[0],2) + Math.pow(arguments[1],2) === Math.pow(arguments[2], 2) ? 
-    `Kwadrat najdłuższego boku jest równy sumie dwóch pozostałych boków co oznacza, że trójkąt jest prostokątny` 
-    : `to nie jest trójkąt prostokątny`;
+    return Math.pow(a,2) + Math.pow(b,2) === Math.pow(c, 2) ? true : false
 }
-console.log(triangle(5,4,3));
+console.log(isRectangularTriangle(3,4,5));
+
