51894d9e1484abc266206ca2267dd1f4247fef47
Nowogrodzki <maciej.nowogrodzki.97@wp.pl>
2020-05-20 15:44:43 +0200

    final version of exercise nr2

--- exercise2.js
+++ exercise2.js
@@ -1,16 +1,17 @@
 // Exercise nr 2 
 
 const isRectangularTriangle = function (x1,x2,x3) {
-    const allArgumentsAreNumbers = Array.from(arguments).some(item => typeof item != 'number')
-    const greateThenZero = Array.from(arguments).some(item => item < 0);
-    const [a,b,c] = Array.from(arguments).sort((a,b) => a - b);
-    if(allArgumentsAreNumbers) {
+    const arrayOfArguments = Array.from(arguments);
+    const areAllArgumentsAreNumbers = arrayOfArguments.some(item => typeof item !== 'number')
+    if(areAllArgumentsAreNumbers) {
         throw new Error('Arguments have to be a numbers');
     }
-    if(greateThenZero) {
+    const areAllGreaterThenZero = arrayOfArguments.some(item => item < 0);
+    if(areAllGreaterThenZero) {
         throw new Error('All arguments have to be greater then 0');
     }
-    return Math.pow(a,2) + Math.pow(b,2) === Math.pow(c, 2) ? true : false
+    const [a,b,c] = arrayOfArguments.sort((a,b) => a - b);
+    return Math.pow(a,2) + Math.pow(b,2) === Math.pow(c,2);
 }
 console.log(isRectangularTriangle(3,4,5));
 
